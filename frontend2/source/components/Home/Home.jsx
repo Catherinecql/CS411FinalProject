8adb14af3a391343439529ecdf7cf789eb17e58a
Catherinecql <qchen50@illinois.edu>
2018-03-25 23:24:17 -0500

    move

---
+++ frontend2/source/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+import styles from './Home.scss'
+
+class Home extends Component {
+
+    render() {
+        return(
+            <div className="Home">
+                <h1>CS411</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ frontend2/source/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css';
+
+// Include your new Components here
+import Home from './components/Home/Home.jsx';
+
+// Include any new stylesheets here
+// Note that components' stylesheets should NOT be included here.
+// They should be 'require'd in their component class file.
+require('./styles/main.scss');
+
+render(
+    <Home />,
+    // Define your router and replace <Home /> with it!
+    document.getElementById('app')
+);
