323f5b5b71e580bbb18b81648ead73f02a1e25c4
jessyelaurie <jessye.laurie@gmail.com>
2018-03-26 01:12:55 -0500

    connect

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,21 @@ var express = require('express'),
 var app = express();
 
 var connection = mysql.createConnection({
-	host:'http://databusters.web.engr.illinois.edu',
-	user:'databusters_admin',
-	password:'databusters',
-	database:'databusters_db'
-	// port:'3306'
+	host:'192.17.90.133',
+  	user: 'databusters_admin',
+ 	password: 'databusters',
+  	database: 'databusters_db',
+  	port:'3306'
 })
+
+
 
 connection.connect(function(err){
 	if(err) throw err;
-	
+
 	console.log("connected to mysql")
 })
+
 var port = process.env.PORT || 7002
 
 app.listen(port)
