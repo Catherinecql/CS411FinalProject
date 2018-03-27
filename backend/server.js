1fac83b0881d42a88d44d2b221341e18ba7847c3
jessyelaurie <jessye.laurie@gmail.com>
2018-03-27 13:03:20 -0500

    new module+some queries

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ var express = require('express'),
     mysql = require('mysql');
 
 var app = express();
+app.use(bodyParser.json())
 
 var connection = mysql.createConnection({
 	host:'192.17.90.133',
@@ -12,27 +13,57 @@ var connection = mysql.createConnection({
 	password:'databusters',
 	database:'databusters_db'
 	//port:'3306'
-})
+});
 
-// connection.connect(function(err){
-// 	if(err) throw err;
-// 	console.log("connected to mysql")	
-	
-// })
+connection.connect(function(err) {
+	if (err) throw err;
+});
+
+
 
 app.get('/everything', function (req, res) {
-	connection.connect(function(err){
-		if(err) throw err;
 		console.log("connected to mysql")	
 		connection.query("select * from Professor",function (error, result,fields){
 			if(error) throw error;
 			console.log(result)
 			res.send(result)
-		})
 	})
 	
-})
+});
 
+app.get('/courses', function (req, res) {
+		console.log("connected to mysql")	
+		connection.query("select * from Professor",function (error, result,fields){
+			if(error) throw error;
+			console.log(result)
+			res.send(result)
+	})
+	
+});
+
+
+app.post('/addCourseToUser', (req, res) => {
+		console.log("connected to mysql");
+		var sql = "insert into takes(username,courses) values ('" + req.body.username + "','" + req.body.title + "')";
+		console.log(sql);
+		connection.query(sql, function(error,result,fields){
+			if(error) throw error;
+			console.log(result)
+			res.send(result)
+	})
+});
+
+app.post('/changeCoursesOfUser', (req, res) => {
+		var courses = req.body.courses; // array of courses
+		var username = req.body.username;
+		var sql = "UPDATE takes SET courses = '" + courses + "' WHERE username = '" + username + "';";
+		console.log(sql);
+		connection.query(sql, function(error,result,fields){
+			if(error) throw error;
+			console.log(result)
+			res.send(result)
+	})
+});
 
 var port = process.env.PORT || 7002
 
