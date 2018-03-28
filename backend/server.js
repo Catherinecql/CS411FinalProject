8230284738907eb999c2ad13d52b0289c5ec0319
Catherinecql <qchen50@illinois.edu>
2018-03-27 23:44:16 -0500

    add allowcrossdomain

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,22 @@ var express = require('express'),
     mysql = require('mysql');
 
 var app = express();
-app.use(bodyParser.json())
+
+// Allow CORS so that backend and frontend could be put on different servers
+var allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+};
+app.use(allowCrossDomain);
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 
 var connection = mysql.createConnection({
 	host:'192.17.90.133',
