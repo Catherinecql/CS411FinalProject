1a6ba4f731a02a9cb43102ba86dd22a3d10723c7
Catherinecql <qchen50@illinois.edu>
2018-04-23 17:51:59 -0500

    submit

--- frontend/source/components/EZSection/EZSection.jsx
+++ frontend/source/components/EZSection/EZSection.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button,Container,Grid,Menu,Dropdown,Input,Segment,Form} from 'semantic-ui-react'
+import { Button,Container,Grid,Menu,Dropdown,Input,Segment,Form,Select} from 'semantic-ui-react'
 import {BrowserRouter as Router, Route, Link,browserHistory,Redirect} from 'react-router-dom';
 import ReactDOM from 'react-dom'
 import _ from 'lodash'
@@ -24,6 +24,11 @@ const inputParsers =
   },
 };
 
+const options = [
+  { key: 'course_no', text: 'Sort by Course NO', value: 'course_no' },
+  { key: 'gpa', text: 'Sort by Course GPA   ', value: 'gpa' }
+]
+
 class EZSection extends Component 
 {
 	constructor(props){
@@ -38,12 +43,15 @@ class EZSection extends Component
 			submittedCourse:'',
 			gpa:'',
 			gpaerror:'',
-			gpaInfo:''
+			gpaInfo:'',
+			select:'gpa'
 		}
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmitgpa = this.handleSubmitgpa.bind(this);
-		this.base_url = 'http://localhost:7002'
+		this.handleSelectChange = this.handleSelectChange.bind(this);
+		// this.base_url = 'http://localhost:7002'
+		this.baseUrl = 'https://mysterious-meadow-13337.herokuapp.com'
 
 	}
 
@@ -55,6 +63,44 @@ class EZSection extends Component
         this.setState({ [name]: value })
         
     }  
+
+    handleSelectChange(event,{value}){
+        // console.log(event)
+        // console.log(event.value);
+        console.log("value",value)
+        // console.log("name",[name])
+        let mode = value
+        let output = this.state.gpaInfo
+			if(mode == 'gpa'){
+				output.sort(function(a,b){
+					if (a.avg > b.avg) {
+						return -1;
+					} else if (a.avg < b.avg) {
+						return 1;
+					}else{
+						return 0
+					}
+
+				})
+			}else{
+				output.sort(function(a,b){
+					if (a.course_number > b.course_number) {
+						return -1;
+					} else if (a.course_number < b.course_number) {
+						return 1;
+					}else{
+						return 0
+					}
+
+				})
+			}
+
+        this.setState(
+        	{ select: value,
+        		gpaInfo :output
+         	}
+        )
+    } 
 
   	handleSubmit(event)
   	{
@@ -76,6 +122,7 @@ class EZSection extends Component
   		let base_url = "https://mysterious-meadow-13337.herokuapp.com/getNextSemProfessors/"
   		let current_url = base_url + department + '/' + Course_NO
   		//"https://mysterious-meadow-13337.herokuapp.com/getNextSemProfessors/"
+  		
   		axios.get(current_url)
 			.then((response) =>
 				{
@@ -99,6 +146,7 @@ class EZSection extends Component
 				)
 
 	  		});
+
   	}
 
   	handleSubmitgpa(event){
@@ -113,22 +161,43 @@ class EZSection extends Component
   		)
 
   		console.log("gpa",gpa)
-  		// console.log("Course_NO",Course_NO)
-  	// 	const input = department+Course_NO
-  	// 	console.log("input",input)
-  	// 	// let base_url = "https://localhost:7002/"
-  	// 	let base_url = "https://mysterious-meadow-13337.herokuapp.com/getNextSemProfessors/"
   		let current_url = this.base_url + '/getMinGPACourses/CS/' + gpa
   	// 	//"https://mysterious-meadow-13337.herokuapp.com/getNextSemProfessors/"
   		axios.get(current_url)
 			.then((response) =>
 				{
 					console.log(response.data);
+					let mode = this.state.select
+					let output = response.data
+					if(mode == 'gpa'){
+						output.sort(function(a,b){
+							if (a.avg > b.avg) {
+								return -1;
+							} else if (a.avg < b.avg) {
+								return 1;
+							}else{
+								return 0
+							}
+
+						})
+					}else{
+						output.sort(function(a,b){
+							if (a.course_number > b.course_number) {
+								return -1;
+							} else if (a.course_number < b.course_number) {
+								return 1;
+							}else{
+								return 0
+							}
+
+						})
+					}
+					console.log("testing",output)
 					//new add:
 					this.setState
 					(
 						{
-							gpaInfo: response.data
+							gpaInfo: output
 						}
 					)
 				}
@@ -138,15 +207,18 @@ class EZSection extends Component
 				this.setState
 				(
 					{
-						gpaInfo : "Sorry! There are no records found :("
+						gpaerror : "Sorry! There are no records found :("
 					}
 				)
 
 	  		});
+	  	
+	  	//sort the score
+
   	}
 
     render() {
-    	const { sectionInfo, error,department,Course_NO,submittedCourse,submittedDepartment,gpa} =this.state
+    	const { sectionInfo, error,department,Course_NO,submittedCourse,submittedDepartment,gpa,gpaerror,gpaInfo} =this.state
     	// console.log("testing",sectionInfo)
     	// console.log(error)
         return(
@@ -189,35 +261,36 @@ class EZSection extends Component
 				       </Segment> 
 			    	}
 			    </Segment>
-			    <Segment className="section" color='purple'>
-		            <Form onSubmit={this.handleSubmitgpa}>
-			        	<Form.Group> 
-			        		<Form.Input
-					        	 name = 'gpa' 
-					        	 value = {gpa}
-					        	 placeholder = "Enter Minimum GPA" 
-					        	 type="text" 
-					        	 onChange = {this.handleChange}
-				        	 />
-
-			        		<Form.Button content = "EZPZ!"/>
-			        	</Form.Group>
-			        </Form>
-
-			        { sectionInfo ? 
-				        <Segment>
-				        	<div> Professor         : {sectionInfo.name_format1} </div>
-				        	<div> Average GPA       : {sectionInfo.avg}</div> 
-				        	<div> </div>
-				        	<div> Rate My Professor :  
-				        		<a href = {sectionInfo.rmp_link}> Click Here!</a>
-				        	</div>
-				        </Segment>
+			    <Segment  color='purple'>
+			      	<Input type='text'
+			      		   name = 'gpa' 
+			      		   value = {gpa} 
+			      		   placeholder = "Enter Minimum GPA" 
+			      		   onChange = {this.handleChange}/>
+			    
+					<Select compact options={options} defaultValue='gpa' onChange = {this.handleSelectChange} />
+					<Button onClick={this.handleSubmitgpa}> EZPZ</Button>
+  					
+			        <div className="gpasection">
+			        { gpaInfo ? 
+			        	(gpaInfo.map((course,j) =>
+			        		<Segment  key={j}>
+			        			<h2> Average GPA       : {course.avg}</h2>
+			        			<div> Course Department : {course.course_department} </div>
+			        			<div> Course NO         : {course.course_number} </div>
+					        
+					        	<div> </div>
+					        	<div> Rate My Professor :  
+					        		<a href = {course.rmp_link}> Click Here!</a>
+					        	</div>
+				        	</Segment>  
+                        ))
 				       : 
 				       <Segment>
-				       	<div> {error} </div>
+				       	<div> {gpaerror} </div>
 				       </Segment> 
 			    	}
+			    	</div>
 			    </Segment>
 		    </div>
 
