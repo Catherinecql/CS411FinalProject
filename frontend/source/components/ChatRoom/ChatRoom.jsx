9f4603db37f0047677eeafac64fee4a6f7273cf2
Catherinecql <qchen50@illinois.edu>
2018-04-22 23:59:13 -0500

    chatroom

--- frontend/source/components/ChatRoom/ChatRoom.jsx
+++ frontend/source/components/ChatRoom/ChatRoom.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
-
+import {Icon, Dropdown,Menu,Segment,Input,Label} from 'semantic-ui-react';
 import SocketIOClient from 'socket.io-client'
+import {BrowserRouter as Router, Route, Link,browserHistory,Redirect} from 'react-router-dom';
+import styles from './ChatRoom.scss'
+import Cookies from 'universal-cookie';
 
 class ChatRoom extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
 			value: '',
-			msg: []
+			msg: [],
+			users:[],
 		}
+		// this.socket = SocketIOClient('https://mysterious-meadow-13337.herokuapp.com')
 		this.socket = SocketIOClient('http://localhost:7002')
 		this.handleChange = this.handleChange.bind(this)
 		this.handleSubmit = this.handleSubmit.bind(this)
-	
+		this.cookies = new Cookies();
 
-	this.socket.on('message', (o) => {
+		this.socket.on('message', (o) => {
 			this.setState({msg: this.state.msg.concat(o)})
+		})
+
+		this.socket.on('users', (o) => {
+			let user = this.state.users
+			user.push(o)
+			this.setState({users: user})
 		})
 	}
 
@@ -25,23 +36,37 @@ class ChatRoom extends React.Component {
 		})
 	}
 	handleSubmit(e){
-		console.log(this.state.value)
-		this.socket.emit('message', this.state.value)
+		const userInfo = this.cookies.get('userInfo')||null
+		this.socket.emit('users',userInfo.username)
+		this.socket.emit('message',this.state.value)
 		this.setState({value: ''})
 		e.preventDefault()
 	}
 	render() {
+		const userInfo = this.cookies.get('userInfo')||null
+    	if(!userInfo){
+    		return(<Redirect to={{pathname:'/', state:{loggedIn: false}}}  push />)
+    	}
+    	console.log(this.state.msg)
+    	console.log("join",this.state.users)
 		return(
 			<div>
-				<ul id="messages">
-					{
-						this.state.msg.map((o) => {
-							return(<li>{o}</li>)
-						})
-					}
-				</ul>
-			      <input id="m" autoComplete="off" value={this.state.value} onChange={this.handleChange} />
-			      <button onClick={this.handleSubmit}>Send </button>
+				<Segment className="room">
+					<div id="messages">
+						{
+							this.state.msg.map((output,j) => {
+								return(
+									<div key={j}>
+										<div> {this.state.users[j]}</div>
+										<Label pointing='left'>{output}</Label>
+									</div>
+								)
+							})
+						}
+					</div>
+					<Input id="m" autoComplete="off" value={this.state.value} onChange={this.handleChange} ></Input>
+				    <button onClick={this.handleSubmit}>Send </button>
+		    	</Segment>
 		    </div>
 		)
 	}
